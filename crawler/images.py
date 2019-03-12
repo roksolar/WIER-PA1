43f46dcf0a835b4f99be097691898cff5d9da406
Blaz Marolt <bm0383@student.uni-lj.si>
2019-03-12 10:30:55 +0100

    Get also images from web pages

--- crawler/images.py
+++ crawler/images.py
@@ -1,2 +1,21 @@
+import os
+import images
+import links
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import requests
+from bs4 import BeautifulSoup
+import urllib.robotparser
+from urllib.parse import urlparse
+
 def get_images(url1):
-    return None
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome('./chromedriver.exe', options=options)
+    driver.get(url1)
+
+    images = driver.find_elements_by_tag_name("img")
+    for image in images:
+        print(image.get_attribute('src'))
+
+    driver.close()
