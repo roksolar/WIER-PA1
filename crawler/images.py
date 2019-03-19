b98df4019f7d95b50de4c5336e02e88a9697b545
Blaz Marolt <bm0383@student.uni-lj.si>
2019-03-19 13:51:00 +0100

    Downloading images...

--- crawler/images.py
+++ crawler/images.py
@@ -7,19 +7,19 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.robotparser
 from urllib.parse import urlparse
+import datetime
 
-def get_images(url1):
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Chrome('./chromedriver.exe', options=options)
-    driver.get(url1)
-
+def get_images(driver):
     images = driver.find_elements_by_tag_name("img")
-    a = []
+    filename = []
+    content_type = []
+    data = []
+    accessed_time =[]
+
     for image in images:
-        print(image.get_attribute('src'))
-        a.append(image.get_attribute('src'))
-
-    driver.close()
-
-    return a
+        filename.append(image.get_attribute('src'))
+        data.append(requests.get(image.get_attribute('src')).content)
+        accessed_time.append(datetime.datetime.now())
+        content_type.append(image.get_attribute('src').split(".")[-1])
+        
+    return [filename, content_type, data, accessed_time]
